4bf6cd7838fd7bff3c4e715bbe95df2965be7ce3
en-i <templebond@icloud.com>
2020-09-08 14:14:13 +0900

    Initial Commit

---
+++ kuroki/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kuroki
+//
+//  Created by terada enishi on 2020/09/08.
+//  Copyright © 2020 terada enishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
